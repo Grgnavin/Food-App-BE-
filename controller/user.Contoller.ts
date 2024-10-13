3678519ea5d415c06e77f032bfa7fb161365ca13
Navin grg <grgnabin60@gmail.com>
2024-10-13 14:07:55 +0545

    Added Authentication

--- controller/user.Contoller.ts
+++ controller/user.Contoller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { User } from "../models/userModel";
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
+import cloudinary from "../utils/cloudinary";
 
 export const signup = async (req: Request, res: Response) => {
     try {
@@ -165,4 +166,92 @@ export const forgotPassword = async (req: Request, res:Response) => {
             message: "Internal Server Error"
         });
     }
-} 
+};
+
+export const resetPassword = async (req:Request, res: Response) => {
+    try {
+        const { token } = req.params;
+        const { newPassword } = req.body;
+
+        const user = await User.findOne({ resetPasswordToken: token, resetPasswordTokenExpiresAt: { $gt: Date.now() } });
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid or expired reset token"
+            })
+        }
+
+        //update password
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordTokenExpiresAt= undefined;
+        await user.save();
+
+        //send reset mail to user 
+        // await send resetsuccess email
+
+        return res.status(200).json({
+            success: true,
+            message: "Password has been reset successfully"
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+}
+
+export const checkAuth = async (req: Request, res: Response) => {
+    try {
+        const userId = req.id;
+
+        const user = await User.findById(userId).select("-password");
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            user
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+}
+
+export const updateProfile = async (req:Request, res: Response) => {
+    try {
+        const userId = req.id;
+        const { fullname, email, address, city, country, profilePicture } = req.body;
+        //upload image on cloudinary
+        let cloudResponse:any;
+        
+        cloudResponse = await cloudinary.uploader.upload(profilePicture);
+        const updatedData = { fullname, email, address, city, country, profilePicture };
+
+        const user = await User.findByIdAndUpdate(userId, updatedData, { 
+            new: true
+        }).select("-password");
+
+        return res.status(200).json({
+            success: true,
+            user,
+            message: "Profile updated successfully"
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
