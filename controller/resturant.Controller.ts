b6d80c4e0c3a2cb1647dc822766dd3a0fe47ea00
Navin grg <grgnabin60@gmail.com>
2024-10-21 23:36:12 +0545

    Added advanced searching functions

--- controller/resturant.Controller.ts
+++ controller/resturant.Controller.ts
@@ -1,8 +1,8 @@
-import { Resturant } from './../models/resturantModel';
 import { Request, Response } from "express";
 import { Resturant } from "../models/resturantModel";
 import { Multer } from "multer";
 import uploadImageOnCloudinary from "../utils/imageUpload";
+import { Order } from "../models/orderModel";
 
 export const createResturant = async (req:Request, res: Response): Promise<void> => {
     try {
@@ -104,5 +104,103 @@ export const updateesturant = async (req:Request, res: Response) => {
         res.status(500).json({
             message: "Internal server error"
         })
+    }
+}
+
+export const getResturantOrder = async (req:Request, res: Response): Promise<void> => {
+    try {
+        const resturant = await Resturant.findOne({ user: req.id });
+        if (!resturant) {
+            res.status(400).json({
+                success: false,
+                message: "Resturant not found"
+            });
+            return;
+        };
+        const orders = await Order.find({ resturant: resturant._id }).populate('resturant').populate('user');
+        res.status(200).json({
+            success: true,
+            orders
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+export const updateOrderStatus = async (req:Request, res: Response) => {
+    try {
+        const { orderId } = req.params;
+        const { status } = req.body;
+        const order = await Order.findById(orderId);
+        if (!order) {
+            res.status(400).json({
+                success: false,
+                message: "Order Not found"
+            });
+            return;
+        }
+        order.status = status;
+        await order.save();
+        res.status(200).json({
+            success: true,
+            message: "Status Updated"
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+}
+
+
+export const searchResturant = async (req:Request, res: Response) => {
+    try {
+        const searchText = req.params.searchText || "";
+        const searchQuery = req.query.searchQuery as string || "";
+        const selectedCuisines = (req.query.searchQuery as string || "").split(",").filter(x=>x);
+        const query: any = {}; 
+        //basic search based on searchText
+        if (searchText) {
+            query.$or = [
+                {
+                    resturantName: { $regex: searchText, $options: "i" },
+                },
+                {
+                    city: { $regex: searchText, $options: "i" },
+                },
+                {
+                    country: { $regex: searchText, $options: "i" }
+                }
+            ]
+        }
+        //filter on the basis of searchQuery
+        if (searchQuery) {
+            query.$or = [
+                {
+                    resturantName: { $regex: searchQuery, $options: "i" },
+                },
+                {
+                    cuisines: { $regex: searchQuery, $options: "i" }
+                }
+            ]
+        }
+        // console.log(query);
+        if (selectedCuisines.length>0) {
+            query.cuisines = { $in: selectedCuisines }
+        }
+        const resturants = await Resturant.find(query);
+        res.status(200).json({
+            success: false,
+            data: resturants
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Internal server error"
+        });
     }
 }
